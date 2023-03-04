d3f511de9fa2936a5918ab6d8cb3ad84f1fc0823
motivatedmind <motivatedmind19@gmail.com>
2023-03-04 19:25:56 +0530

    push uncommited changes due to laptop back up

--- src/main/java/com/devglan/config/AuthorizationServerConfig.java
+++ src/main/java/com/devglan/config/AuthorizationServerConfig.java
@@ -15,6 +15,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	static final String CLIEN_ID = "devglan-client";
 	static final String CLIENT_SECRET = "devglan-secret";
+	//static final String CLIENT_SECRET = "$2a$04$e/c1/RfsWuThaWFCrcCuJeoyvwCV0URN/6Pn9ZFlrtIWaU/vj/BfG";
 	static final String GRANT_TYPE_PASSWORD = "password";
 	static final String AUTHORIZATION_CODE = "authorization_code";
     static final String REFRESH_TOKEN = "refresh_token";
